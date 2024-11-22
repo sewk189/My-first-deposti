8c5dcb7ffc0a6b9839ec67e02acca2037ab9a7fd
sewk189 <sewkwaiseng@gmail.com>
2024-11-22 17:55:31 +0800

    tretr

---
+++ Learningmmy/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Worlllld!");
+        System.out.print("Hello, Worlllld!");
+        System.out.println("Hello, Worlllld!");
+        
+    }
+}
